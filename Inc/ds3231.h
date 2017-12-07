5563877aa9982fd20adac7491c8cf50c68372462
Mateusz Salamon <mateusz.salamon91@gmail.com>
2017-12-07 21:32:10 +0100

    Begin library.

---
+++ Inc/ds3231.h
@@ -0,0 +1,13 @@
+/*
+ * ds3231.h
+ *
+ *  Created on: 7 gru 2017
+ *      Author: Mateusz Salamon
+ */
+
+#ifndef DS3231_H_
+#define DS3231_H_
+
+
+
+#endif /* DS3231_H_ */
---
+++ Src/ds3231.c
@@ -0,0 +1,14 @@
+/*
+ * ds3231.c
+
+ *
+ *  Created on: 7 gru 2017
+ *      Author: Mateusz Salamon
+ */
+#include "main.h"
+#include "stm32f1xx_hal.h"
+#include "i2c.h"
+#include "gpio.h"
+
+#include "ds3231.h"
+
